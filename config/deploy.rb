b970c81943cd29e033f5b475c2bf81dd83389d01
togglecodekhandakar <31459568+togglecodekhandakar@users.noreply.github.com>
2018-01-15 11:47:20 +1100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.1"
 
 set :application, "Permit"
-set :repo_url, "git@github.com:togglecodekhandakar/permitadmin.git"
+set :repo_url, "git@github.com:togglecodekhandakar/ec2_deploy.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
